0735ad2fff255ac92f5ea21198c1c5030f5dd67c
rorosyaraswati <rorosyaraswati@gmail.com>
2024-06-01 00:36:21 +0700

    change the index.js to app.js

--- app.js
+++ app.js
@@ -2,22 +2,28 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import http from "http"; // Tambahkan ini
+
 import db from "./config/database.js";
 import router from "./routes/index.js";
+
 dotenv.config();
 const app = express();
 
-try{
+try {
     await db.authenticate();
     console.log('Database Connect');
-} catch (error){
+} catch (error) {
     console.error(error);
 }
 
-
-app.unsubscribe(cors({ credentials:true, origin:'http:localhost:3000'})) //ubah sesuai server
+app.use(cors({ credentials:true, origin:'http://localhost:3000'})); // Ubah ini
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
-app.listen(5000, ()=> console.log('Server running at port 5000'));
+const server = http.createServer(app); // Ubah ini
+const PORT = 5000; // Ubah ini
+server.listen(PORT, () => console.log(`Server running at port ${PORT}`));
+
+
